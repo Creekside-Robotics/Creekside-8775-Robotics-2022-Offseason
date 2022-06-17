f9f3c1d8c1e4018498ae2f9ec19f47993033b8ae
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2022-06-17 16:13:24 -0400

    Changing Arm Component to Abstract Class

--- src/main/java/frc/robot/subsystems/ArmComponent.java
+++ src/main/java/frc/robot/subsystems/ArmComponent.java
@@ -4,42 +4,31 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class ArmComponent extends SubsystemBase {
+public abstract class ArmComponent extends SubsystemBase {
   /** Creates a new ArmComponent. */
   private CANSparkMax mainMotor;
   private double multiplier;
-  private double encoderMultiplier;
-  private Encoder encoder;
 
-  //Sensor input
-  public double position;
 
-  public ArmComponent(int deviceId, MotorType type, double multiplier, double revolutionsInRange, double encoderMultiplier, int port1, int port2) {
+  public ArmComponent(int deviceId, MotorType type, double multiplier) {
     this.mainMotor = new CANSparkMax(deviceId, type);
     this.multiplier = multiplier;
-    this.encoderMultiplier = encoderMultiplier;
-    this.encoder = new Encoder(port1, port2);
-    this.encoder.setDistancePerPulse(1/(Constants.tickPerRev * revolutionsInRange));
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    this.position = this.encoder.getDistance() * this.encoderMultiplier;
   }
 
   public void setSpeed(double speed){
     this.mainMotor.set(speed * this.multiplier);
   }
 
-  public void reset(){
-    this.encoder.reset();;
-  }
+  abstract void resetPosition();
+
+  abstract double getPostion();
 }
