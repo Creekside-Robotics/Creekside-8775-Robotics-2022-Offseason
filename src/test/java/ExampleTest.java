9da28fdba25904bfd9003629581af88f561e6bf1
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2022-07-01 15:02:15 -0400

    Example Test

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ExampleTest {
+
+    @Test
+    public void twoPlusTwoIsFour() {
+        assertEquals(2 + 2, 4);
+    }
+}
