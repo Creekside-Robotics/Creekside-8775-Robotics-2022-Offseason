62cd22fd6de7a618f4bdd3b04a522b2cc76c0eb4
rtejanitta <91956988+rtejanitta@users.noreply.github.com>
2022-07-19 15:18:53 -0400

    RunFlywheel.java
    Changed values from speed to voltage.

--- src/main/java/frc/robot/commands/RunFlywheel.java
+++ src/main/java/frc/robot/commands/RunFlywheel.java
@@ -7,11 +7,11 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class RunFlywheel extends CommandBase {
   @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
   private final Shooter shooterSubsystem;
-  private double flywheelSpeed;
+  private double flywheelVoltage; //Changed flywheelSpeed to flywheelVoltage so that it is consistent with battery voltage sag 
 
-  public RunFlywheel(Shooter subsystem, double speed) {
+  public RunFlywheel(Shooter subsystem, double voltage) {
     this.shooterSubsystem = subsystem;
-    this.flywheelSpeed = speed;
+    this.flywheelVoltage = voltage; //Same change as line 10 as well as a change in parameter for consistency
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(this.shooterSubsystem);
   }
@@ -24,7 +24,7 @@ public class RunFlywheel extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    this.shooterSubsystem.setSpeed(this.flywheelSpeed);
+    this.shooterSubsystem.setFlywheelVoltage(this.flywheelVoltage); //Executing changes made on line 10 and 14
   }
 
   // Called once the command ends or is interrupted.
