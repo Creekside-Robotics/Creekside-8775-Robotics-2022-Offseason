938762f3d0be95c47233f23f06c94fa2d3d183a9
rtejanitta <91956988+rtejanitta@users.noreply.github.com>
2022-07-23 11:01:35 -0400

    Add files via upload

---
+++ src/main/java/frc/robot/subsystems/TargetCamera.java
@@ -0,0 +1,22 @@
+import frc.robot.subsystems;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class TargetCamera extends AbstractCamera {
+    
+    public TargetCamera(){
+        super.AbstractCamera(); // Calling to the parent class to define variables
+
+    }
+}
+
+
+
