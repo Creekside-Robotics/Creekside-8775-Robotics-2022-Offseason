a3dd224cf4682e0cc6476241856f87cb2118698d
rtejanitta <91956988+rtejanitta@users.noreply.github.com>
2022-07-27 14:53:04 -0400

    Commented GoalCamera class

--- src/main/java/frc/robot/subsystems/GoalCamera.java
+++ src/main/java/frc/robot/subsystems/GoalCamera.java
@@ -19,28 +19,32 @@ public class GoalCamera extends AbstractCamera{
     private static double goalCameraPitch = 2.0;
     private static Translation2d goalCameraOffset = new Translation2d(2.0,2.0);
     private static double goalHeight = 2.0; 
-    private static double distanceTogoal = 2.0;
+    private static double distanceToGoal = 2.0;
 
 
     public GoalCamera() {
         super(goalCameraName, goalPipelineIndex, goalCameraHeight, goalCameraPitch, goalCameraOffset, goalHeight);
-        //TODO Auto-generated constructor stub
-        
     }
 
     @Override
     public Trajectory getTrajectory() {
-        // TODO Auto-generated method stub
-        var goalTranslation = getRelativeTranslation();
-        var robotPosition = new Pose2d();
+    
+        var goalTranslation = getRelativeTranslation(); //position of the goal
+
+        var robotPosition = new Pose2d(); //initializes to (0,0,0)
+
+        //Takes the vector of the goal and turns it into a pose that is a specific distance (distanceToGoal) away from the robot
         var finalAngle = new Rotation2d(goalTranslation.getX(),goalTranslation.getY());
         var unitVector = goalTranslation.div(goalTranslation.getNorm());
-        var multipliedUnitVector = unitVector.times(distanceTogoal);
+        var multipliedUnitVector = unitVector.times(distanceToGoal);
         var finalDisplacement = goalTranslation.minus(multipliedUnitVector);
         var finalPose = new Pose2d(finalDisplacement,finalAngle);
+
+        //Creating the trajectory (interiorWaypoints is an empty array)
         var interiorWaypoints = new ArrayList<Translation2d>();
         var trajectoryConfig = new TrajectoryConfig(Constants.kMaxSpeedMetersPerSecond,Constants.kMaxAccelerationMetersPerSecondSquared);
         var finalTrajectory = TrajectoryGenerator.generateTrajectory(robotPosition, interiorWaypoints, finalPose, trajectoryConfig);
+
         return finalTrajectory;
         
 
