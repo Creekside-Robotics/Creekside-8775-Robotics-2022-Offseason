a48022412856cb4663f96aa3a37588a1134bf3b2
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2022-08-14 18:13:43 -0400

    Added Pose Test

--- src/test/java/BallTrajectoryTest.java
+++ src/test/java/BallTrajectoryTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.*;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,5 +20,13 @@ public class BallTrajectoryTest {
     public void nullInstanceCheck() {
         var trajectoryToBall = new BallCamera().new BallCameraTrajectory(null, 0).getTrajectory();
         assertNull(trajectoryToBall);
+    }
+
+    @Test
+    public void finalPoseCheck() {
+        var ballTranslation = new Translation2d(3, -6);
+        var expectedEndPose = new Pose2d(ballTranslation, new Rotation2d(ballTranslation.getX(), ballTranslation.getY()));
+        var actualEndPose = new BallCamera().new BallCameraTrajectory(ballTranslation, 0).getEndPose();
+        assertEquals(expectedEndPose, actualEndPose);
     }
 }
