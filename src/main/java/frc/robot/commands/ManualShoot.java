48a88b89ba397f5c40a395c8111f7e085a074d4e
rtejanitta <91956988+rtejanitta@users.noreply.github.com>
2022-08-28 17:30:15 -0400

    Cleaned up unnecessary imports

--- src/main/java/frc/robot/commands/ManualShoot.java
+++ src/main/java/frc/robot/commands/ManualShoot.java
@@ -6,13 +6,11 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import frc.robot.Constants;
-import frc.robot.commands.RunFlywheel;
-import frc.robot.commands.RunIntake;
+
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
