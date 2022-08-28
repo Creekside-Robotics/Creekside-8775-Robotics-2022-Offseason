2cd0ac346373f069c0e1c9b9b91c6e79ffb74a59
rtejanitta <91956988+rtejanitta@users.noreply.github.com>
2022-08-28 17:30:15 -0400

    Cleaned up unnecessary imports

--- src/main/java/frc/robot/commands/AutoShoot.java
+++ src/main/java/frc/robot/commands/AutoShoot.java
@@ -12,7 +12,6 @@ import frc.robot.subsystems.Shooter;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
